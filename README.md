3b1c42e3cbc2c567f026ae3c40a9f7607f7202f7
iannisdezwart <iannis__@hotmail.com>
2021-11-07 22:56:07 +0100

    🚀 First Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# LaTeX API
+
+A simple API that converts LaTeX equations to SVG.
+
+### dependencies
+Make sure you have all dependencies installed:
+
+```sh
+sudo apt install texlive texlive-science
+```
---
+++ index.ts
@@ -0,0 +1,68 @@
+import * as fs from 'fs'
+import { createAPI, readBody } from '@iannisz/node-api-kit'
+import { randomBytes } from 'crypto'
+import { exec } from 'child_process'
+
+const PORT = +process.argv[2] || 3000
+
+const wrapLatex = (latex: string) => `\
+\\documentclass[12pt]{article}
+\\usepackage{amsmath}
+\\usepackage{amssymb}
+\\usepackage{amsfonts}
+\\usepackage{xcolor}
+\\usepackage{siunitx}
+\\usepackage[utf8]{inputenc}
+\\thispagestyle{empty}
+\\begin{document}
+${ latex.trim() }
+\\end{document}
+`
+
+const compileLatex = (
+	tempDir: string
+) => new Promise<void>((resolve, reject) => {
+	const cmd = `cd ${ tempDir } && latex file.tex && dvisvgm --no-fonts file.dvi`
+
+	exec(cmd, (err, _stdout, stderr) => {
+		if (err) {
+			reject(stderr)
+			return
+		}
+
+		resolve()
+	})
+})
+
+const api = createAPI(PORT)
+
+api.get('/render', async (req, res) => {
+	const latex = wrapLatex(await readBody(req))
+	const id = randomBytes(16).toString('hex')
+	const tempDir = `temp/${ id }`
+
+	// Store the LaTeX equation into a file.
+
+	fs.mkdirSync(tempDir, { recursive: true })
+	fs.writeFileSync(`${ tempDir }/file.tex`, latex)
+
+	// Compile the LaTex equation to an svg file.
+
+	try {
+		await compileLatex(tempDir)
+	}
+	catch {
+		res.statusCode = 500
+		res.end('Error while compiling LaTeX equation')
+	}
+
+	// Stream the svg output to the client.
+
+	fs.createReadStream(`${ tempDir }/file.svg`).pipe(res)
+
+	// Delete the temp directory.
+
+	res.on('finish', () => {
+		fs.rmdirSync(tempDir, { recursive: true })
+	})
+})
